a247f2db4b48b2d11371d984348f51eb54e2c4d3
Sachin <sachingramtekkar@gmail.com>
2025-05-27 13:50:09 +0530

    test

--- src/App.js
+++ src/App.js
@@ -11,21 +11,21 @@ import ProductList from "./ProductList";
 import SearchProducts from "./SearchProducts";
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        {/* <Header /> */}
-        <Routes>
-          <Route path="/login" Component={Login}></Route>
-          <Route path="/Register" Component={Register}></Route>
-          <Route path="/add" Component={AddProduct}></Route>
-          <Route path="/" Component={ProductList}></Route>
-          <Route path="/search" Component={SearchProducts}></Route>
-          <Route path="/update/:id" Component={UpdateProduct}></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                {/* <Header /> */}
+                <Routes>
+                    <Route path="/login" Component={Login}></Route>
+                    <Route path="/Register" Component={Register}></Route>
+                    <Route path="/add" Component={AddProduct}></Route>
+                    <Route path="/" Component={ProductList}></Route>
+                    <Route path="/search" Component={SearchProducts}></Route>
+                    <Route path="/update/:id" Component={UpdateProduct}></Route>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
